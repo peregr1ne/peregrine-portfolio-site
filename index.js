6fc8715077b00007fb56cd986a4fdfada472dc18
peregr1ne <61361793+peregr1ne@users.noreply.github.com>
2026-02-05 10:47:44 +0100

    anim fix

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 // simple timeout animation for servicecard
 
-const cards = document.querySelectorAll('.service-card');
+// const cards = document.querySelectorAll('.service-card');
+
+//   const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//       if (entry.isIntersecting) {
+//         cards.forEach((card, index) => {
+//           setTimeout(() => card.classList.add('visible'), index * 200);
+//         });
+//         observer.disconnect(); // animation played only once
+//       }
+//     });
+//   }, { threshold: 0.2 });
+
+//   observer.observe(document.querySelector('.services-card-container'));
+
+  const cards = document.querySelectorAll(".service-card");
 
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        cards.forEach((card, index) => {
-          setTimeout(() => card.classList.add('visible'), index * 200);
-        });
-        observer.disconnect(); // animation played only once
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target); // animation's played only once
       }
     });
-  }, { threshold: 0.2 });
+  }, { threshold: 0.3 });
 
-  observer.observe(document.querySelector('.services-card-container'));
+  cards.forEach((card) => observer.observe(card));
