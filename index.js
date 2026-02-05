4cb076976d9fa483329308f3b98120cd78fea6fb
peregr1ne <61361793+peregr1ne@users.noreply.github.com>
2026-02-05 13:28:58 +0100

    added simple running line animation

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
 // simple timeout animation for servicecard
-
-// const cards = document.querySelectorAll('.service-card');
-
-//   const observer = new IntersectionObserver((entries) => {
-//     entries.forEach((entry) => {
-//       if (entry.isIntersecting) {
-//         cards.forEach((card, index) => {
-//           setTimeout(() => card.classList.add('visible'), index * 200);
-//         });
-//         observer.disconnect(); // animation played only once
-//       }
-//     });
-//   }, { threshold: 0.2 });
-
-//   observer.observe(document.querySelector('.services-card-container'));
-
   const cards = document.querySelectorAll(".service-card");
 
   const observer = new IntersectionObserver((entries) => {
@@ -27,3 +11,31 @@
   }, { threshold: 0.3 });
 
   cards.forEach((card) => observer.observe(card));
+
+
+  // running line services animation
+  document.addEventListener("DOMContentLoaded", () => {
+  const track = document.getElementById("runningTrack");
+
+  // double content
+  track.innerHTML += track.innerHTML;
+
+  let speed = 1; // pixels movement speed per frame
+  let x = 0;
+
+  function animate() {
+    x -= speed;
+
+    // track width (half of track after doubling)
+    const trackWidth = track.scrollWidth / 2;
+
+    if (-x >= trackWidth) {
+      x = 0; // nearly smooth reset
+    }
+
+    track.style.transform = `translateX(${x}px)`;
+    requestAnimationFrame(animate);
+  }
+
+  animate();
+});
